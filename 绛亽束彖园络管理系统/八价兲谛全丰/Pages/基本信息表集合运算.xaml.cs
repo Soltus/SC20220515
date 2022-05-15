0f9acd5d1b5a8852070194ded9433e4d0aaa4d42
viviSoltus <liaoshanyi@qq.com>
2022-05-16 03:59:26 +0800

    新增

---
+++ 绛亽束彖园络管理系统/八价兲谛全丰/Pages/基本信息表集合运算.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 绛亽束彖园络管理系统
+{
+    /// <summary>
+    /// 基本信息表集合运算.xaml 的交互逻辑
+    /// </summary>
+    public partial class 基本信息表集合运算 : Page
+    {
+        public 基本信息表集合运算()
+        {
+            InitializeComponent();
+        }
+    }
+}
